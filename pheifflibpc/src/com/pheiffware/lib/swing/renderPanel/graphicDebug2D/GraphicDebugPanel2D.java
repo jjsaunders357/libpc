e2c5a06cc6c381fb35c3d681f6c105753e43ed19
Steve <Steve@Steve-PC.fios-router.home>
2015-09-19 01:23:10 -0400

    Improved how GraphicDebugPanel2D handles synchronization.

--- pheifflibpc/src/com/pheiffware/lib/swing/renderPanel/graphicDebug2D/GraphicDebugPanel2D.java
+++ pheifflibpc/src/com/pheiffware/lib/swing/renderPanel/graphicDebug2D/GraphicDebugPanel2D.java
@@ -5,8 +5,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.swing.SwingUtilities;
-
 import com.pheiffware.lib.swing.graphics.G2D;
 import com.pheiffware.lib.swing.graphics.G2DRender;
 import com.pheiffware.lib.swing.renderPanel.RenderPanel;
@@ -14,20 +12,18 @@ import com.pheiffware.lib.swing.renderPanel.RenderPanel;
 public class GraphicDebugPanel2D extends RenderPanel
 {
 	private static final long serialVersionUID = 5990924595581860103L;
+	private final Object renderLock = new Object();
 	private final List<Object> renderItems = new LinkedList<>();
 	private final List<Color> renderColors = new LinkedList<>();
 
 	public GraphicDebugPanel2D(int width, int height, double renderPeriod)
 	{
 		super(width, height, renderPeriod);
-		startRender();
 	}
 
-	public GraphicDebugPanel2D(int width, int height, double renderPeriod,
-			double viewX, double viewY, double viewWidth, double viewHeight)
+	public GraphicDebugPanel2D(int width, int height, double renderPeriod, double viewX, double viewY, double viewWidth, double viewHeight)
 	{
 		super(width, height, renderPeriod, viewX, viewY, viewWidth, viewHeight);
-		startRender();
 	}
 
 	/**
@@ -37,19 +33,15 @@ public class GraphicDebugPanel2D extends RenderPanel
 	 */
 	public void setRenderables(Object[] objects, Color[] colors)
 	{
-		SwingUtilities.invokeLater(new Runnable()
+		synchronized (renderLock)
 		{
-			@Override
-			public void run()
+			renderItems.clear();
+			for (int i = 0; i < objects.length; i++)
 			{
-				renderItems.clear();
-				for (int i = 0; i < objects.length; i++)
-				{
-					renderItems.add(objects[i]);
-					renderColors.add(colors[i]);
-				}
+				renderItems.add(objects[i]);
+				renderColors.add(colors[i]);
 			}
-		});
+		}
 	}
 
 	/**
@@ -59,41 +51,40 @@ public class GraphicDebugPanel2D extends RenderPanel
 	 */
 	public void addRenderable(Object object, Color color)
 	{
-		SwingUtilities.invokeLater(new Runnable()
+		synchronized (renderLock)
 		{
-			@Override
-			public void run()
-			{
-				renderItems.add(object);
-				renderColors.add(color);
-			}
-		});
+			renderItems.add(object);
+			renderColors.add(color);
+		}
 	}
 
 	public void clearRenderables()
 	{
-		SwingUtilities.invokeLater(new Runnable()
+		synchronized (renderLock)
 		{
-			@Override
-			public void run()
-			{
-				renderItems.clear();
-				renderColors.clear();
-			}
-		});
+			renderItems.clear();
+			renderColors.clear();
+		}
 	}
 
 	@Override
 	public void render(G2D g2d)
 	{
 		g2d.fillRectAbsolute(0, 0, getWidth(), getHeight());
-		g2d.setBackground(new Color(255, 0, 0));
-		Iterator<Color> colorIter = renderColors.iterator();
-		for (Object object : renderItems)
-		{
-			g2d.setColor(colorIter.next());
-			G2DRender.render(g2d, object);
-		}
+		g2d.setColor(new Color(255, 0, 0));
+		renderDebugObjects(g2d);
 	}
 
+	protected void renderDebugObjects(G2D g2d)
+	{
+		synchronized (renderLock)
+		{
+			Iterator<Color> colorIter = renderColors.iterator();
+			for (Object object : renderItems)
+			{
+				g2d.setColor(colorIter.next());
+				G2DRender.render(g2d, object);
+			}
+		}
+	}
 }
